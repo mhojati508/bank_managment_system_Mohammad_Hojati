835f33dd5d79098f37725c0039411e615e460e0a
mhojati508 <mhojati508@gmail.com>
2025-11-22 22:32:47 +0330

    Update core.py

--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+#salam ostad vaght bekheir. kheli mamanon az nazarat arzeshmandetun. eslahati ke farmude budid anjam dadam be joz Note4
+#ke motasefaneh koli fekr kardam vali natunestam be natijeh beresam. mamnoon misham bishtar rahnamii befrmaiid. 
 '''
 APM: Kheyli awli
 
@@ -36,18 +38,13 @@ from models import Customer, Account, Transaction
 import numpy as np
 import random
 
+
 class AdminPanel:
     def __init__(self):
         self.session = get_session()
 
-    def card_number(self):
-        digits = np.random.randint(0, 9, 16)
-        card_number = "".join(map(str, digits))
-        return card_number
 
-
-
-    def create_customer(self, name, email, age, phone, address):
+    def create_customer(self, name: str, email: str, age: int, phone: str, address: str)-> Customer:
         # row tooye database besazam
 
         # ^^^
@@ -61,7 +58,25 @@ class AdminPanel:
         print(f'customer {name} created successfully')
         return customer
 
-    def create_account(self, customer_id, account_type, balance, pin, card_number=None ):
+    def card_number(self):
+        digits = np.random.randint(0, 9, 16)
+        card_number = "".join(map(str, digits))
+        return card_number
+        """
+        #way1
+               def card_number(self):
+                    digits = np.random.randint(0, 9, 16)
+                    card_number = "".join(map(str, digits))
+                    return card_number
+               card_number = card_number()
+               return card_number
+         #way2      
+         def card_number(self):
+             return "".join(str(random.randit(0,9)) for _ in range(16))
+
+        """
+
+    def create_account(self, customer_id: int, account_type: str, balance: float, pin: str, card_number: str=None )-> Account:
         customer = self.session.get(Customer, customer_id)
         # row ro bekesham biron
 
@@ -74,30 +89,28 @@ class AdminPanel:
         # card number shoam ezafe kon?????
         # numpy ---> gpt beporsid ???
         # card_number=328273817
-        # def card_number(self):
-        #     digits = np.random.randint(0, 9, 16)
-        #     card_number = "".join(map(str, digits))
-        #     return card_number
 
-        # def card_number(self):
-        #     return "".join(str(random.randit(0,9)) for _ in range(16))
 
         hashed_pin = hash_password(pin)
+
         if card_number is None:
             create_card_number=self.card_number()
-
-
-
+                                                                            
         account = Account(balance=balance, type=account_type, pin=hashed_pin, card_number=create_card_number)
         self.session.add(account)
         self.session.commit()
-
+        print(f"account {account_type} created successfully")
         return account
 
     # -------
 
-    def show_balance(self, account_id):
+    def show_balance(self, account_id: int)-> float:
         account = self.session.get(Account, account_id)
+
+        # if not account:
+        #     error_msg = f'Account with id {account_id} not found'
+        #     print(error_msg)
+        #     raise Exception(error_msg)
         if not account:
             # ^^
             print(f'account {account_id} not found')
@@ -109,43 +122,47 @@ class AdminPanel:
         print(f'account balance: {balance}')
         return balance
 
-    def deposit(self, account_id, amount):
+    def deposit(self, account_id: int, amount: float)-> Account:
+        
         account = self.session.get(Account, account_id)
         if not account:
             # ^^
             print(f'account {account_id} not found')
             raise Exception(f'Account with id {account_id} not found')
+        
+        elif amount > 0:
+            account.balance = account.balance + amount
+            self.session.commit()
+            return account
+        else:
+            raise ValueError("Deposit amount must be positive")
+        
 
-        account.balance = account.balance + amount
-        self.session.commit()
-        return account
-
-    def withdraw(self, account_id, amount):
+    def withdraw(self, account_id: int, amount: float)-> Account:
         # ^^^^^^^^
         # '''
         # hatman check kone amount <balance
         # '''
+        
         account = self.session.get(Account, account_id)
         if not account:
             print(f'account {account_id} not found')
             raise Exception(f'Account with id {account_id} not found')
-
+      
         elif amount < account.balance:
             account.balance = account.balance - amount
             self.session.commit()
-            #** print ha bayad ghable return bashand ta ejra beshe
             print(f"your account balance is: {account.balance}")
             return account
-        #print()
-        #print(f"your account balance is: {account.balance}")
+    
         else:
             print('your balance is not enough for withdraw')
             return None
     
 
-
-    def transfer(self, from_account_id, to_account_id, amount):
+    def transfer(self, from_account_id: int, to_account_id: int, amount: float)-> tuple[Account, Account, Account]:
         from_account = self.session.get(Account, from_account_id)
+        
         if not from_account:
             print(f'account {from_account_id} not found')
             raise Exception(f'Account with id {from_account_id} not found')
@@ -153,11 +170,23 @@ class AdminPanel:
         if not to_account:
             print(f'To-account {to_account_id} not found')
             raise Exception(f'Account with id {to_account_id} not found')
-        if  amount < from_account.balance:
+        
+        if from_account_id == to_account_id:
+            raise ValueError("Cannot transfer to the same account")
+        
+        if amount <= 0:
+            raise ValueError("Transfer amount must be positive")
+
+        
+        elif  amount < from_account.balance:
             from_account.balance -= amount
             to_account.balance += amount
             self.session.commit()
-            return {'withdraw from': from_account_id, 'deposit to': to_account_id, 'amount': amount}
+            return f'withdraw from {from_account_id}, deposit to {to_account_id}, amount {amount} '
+        
+        else:
+            print('your balance is not enough for transfer')
+            return None
         
 
     # '''
@@ -166,9 +195,8 @@ class AdminPanel:
 
 
 
-    def show_transaction(self, account_id):
+    def show_transaction(self, account_id: int)-> list[Account]:
         account = self.session.get(Account, account_id)
         if not account:
             print(f'account {account_id} not found')
             raise Exception(f'Account with id {account_id} not found')
-        return account.balance
